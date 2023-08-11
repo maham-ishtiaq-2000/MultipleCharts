0360dfb600339254216c542afa27e00de321d1cb
maham-ishtiaq-2000 <mahamishtiaq2000@gmail.com>
2023-08-11 02:21:01 -0700

    myfirstevercommit

---
+++ script.js
@@ -0,0 +1,629 @@
+var data = [
+    {
+      "": 2022,
+      "__1": "6/1/2022",
+      "Starts": 10,
+      "__2": 10,
+      "__3": 0,
+      "__4": 0,
+      "__5": 1,
+      "__6": 1
+    },
+    {
+      "": 2022,
+      "__1": "7/1/2022",
+      "Starts": 15,
+      "__2": 25,
+      "__3": 0,
+      "__4": 0,
+      "__5": 0,
+      "__6": 1
+    },
+    {
+      "": 2022,
+      "__1": "8/1/2022",
+      "Starts": 20,
+      "__2": 45,
+      "__3": 0,
+      "__4": 0,
+      "__5": 0,
+      "__6": 1
+    },
+    {
+      "": 2022,
+      "__1": "9/1/2022",
+      "Starts": 20,
+      "__2": 65,
+      "__3": 5,
+      "__4": 5,
+      "__5": 3,
+      "__6": 4
+    },
+    {
+      "": 2022,
+      "__1": "10/1/2022",
+      "Starts": 20,
+      "__2": 85,
+      "__3": 7,
+      "__4": 12,
+      "__5": 21,
+      "__6": 25
+    },
+    {
+      "": 2022,
+      "__1": "11/1/2022",
+      "Starts": 20,
+      "__2": 105,
+      "__3": 10,
+      "__4": 22,
+      "__5": 7,
+      "__6": 32
+    },
+    {
+      "": 2022,
+      "__1": "12/1/2022",
+      "Starts": 20,
+      "__2": 125,
+      "__3": 12,
+      "__4": 34,
+      "__5": 24,
+      "__6": 56
+    },
+    {
+      "": 2023,
+      "__1": "1/1/2023",
+      "Starts": 57,
+      "__2": 182,
+      "__3": 16,
+      "__4": 50,
+      "__5": 23,
+      "__6": 79
+    },
+    {
+      "": 2023,
+      "__1": "2/1/2023",
+      "Starts": 45,
+      "__2": 227,
+      "__3": 18,
+      "__4": 68,
+      "__5": 26,
+      "__6": 105
+    },
+    {
+      "": 2023,
+      "__1": "3/1/2023",
+      "Starts": 60,
+      "__2": 287,
+      "__3": 22,
+      "__4": 90,
+      "__5": 23,
+      "__6": 128
+    },
+    {
+      "": 2023,
+      "__1": "4/1/2023",
+      "Starts": 50,
+      "__2": 337,
+      "__3": 10,
+      "__4": 100,
+      "__5": 13,
+      "__6": 141
+    },
+    {
+      "": 2023,
+      "__1": "5/1/2023",
+      "Starts": 40,
+      "__2": 377,
+      "__3": 15,
+      "__4": 115,
+      "__5": 15,
+      "__6": 156
+    },
+    {
+      "": 2023,
+      "__1": "6/1/2023",
+      "Starts": 211,
+      "__2": 588,
+      "__3": 36,
+      "__4": 151,
+      "__5": 0,
+      "__6": 156
+    },
+    {
+      "": 2023,
+      "__1": "7/1/2023",
+      "Starts": 50,
+      "__2": 638,
+      "__3": 9,
+      "__4": 160,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2023,
+      "__1": "8/1/2023",
+      "Starts": 72,
+      "__2": 710,
+      "__3": 14,
+      "__4": 174,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2023,
+      "__1": "9/1/2023",
+      "Starts": 40,
+      "__2": 750,
+      "__3": 15,
+      "__4": 189,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2023,
+      "__1": "10/1/2023",
+      "Starts": 59,
+      "__2": 809,
+      "__3": 26,
+      "__4": 215,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2023,
+      "__1": "11/1/2023",
+      "Starts": 66,
+      "__2": 875,
+      "__3": 13,
+      "__4": 228,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2023,
+      "__1": "12/1/2023",
+      "Starts": 74,
+      "__2": 949,
+      "__3": 15,
+      "__4": 243,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2024,
+      "__1": "1/1/2024",
+      "Starts": 118,
+      "__2": 1067,
+      "__3": 15,
+      "__4": 258,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2024,
+      "__1": "2/1/2024",
+      "Starts": 127,
+      "__2": 1194,
+      "__3": 33,
+      "__4": 291,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2024,
+      "__1": "3/1/2024",
+      "Starts": 139,
+      "__2": 1333,
+      "__3": 37,
+      "__4": 328,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2024,
+      "__1": "4/1/2024",
+      "Starts": 139,
+      "__2": 1472,
+      "__3": 31,
+      "__4": 359,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2024,
+      "__1": "5/1/2024",
+      "Starts": 93,
+      "__2": 1565,
+      "__3": 55,
+      "__4": 414,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2024,
+      "__1": "6/1/2024",
+      "Starts": 78,
+      "__2": 1643,
+      "__3": 48,
+      "__4": 462,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2024,
+      "__1": "7/1/2024",
+      "Starts": 78,
+      "__2": 1721,
+      "__3": 61,
+      "__4": 523,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2024,
+      "__1": "8/1/2024",
+      "Starts": 113,
+      "__2": 1834,
+      "__3": 77,
+      "__4": 600,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2024,
+      "__1": "9/1/2024",
+      "Starts": 102,
+      "__2": 1936,
+      "__3": 90,
+      "__4": 690,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2024,
+      "__1": "10/1/2024",
+      "Starts": 151,
+      "__2": 2087,
+      "__3": 105,
+      "__4": 795,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2024,
+      "__1": "11/1/2024",
+      "Starts": 145,
+      "__2": 2232,
+      "__3": 110,
+      "__4": 905,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2024,
+      "__1": "12/1/2024",
+      "Starts": 154,
+      "__2": 2386,
+      "__3": 120,
+      "__4": 1025,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2025,
+      "__1": "1/1/2025",
+      "Starts": 143,
+      "__2": 2529,
+      "__3": 128,
+      "__4": 1153,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2025,
+      "__1": "2/1/2025",
+      "Starts": 106,
+      "__2": 2635,
+      "__3": 146,
+      "__4": 1299,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2025,
+      "__1": "3/1/2025",
+      "Starts": 72,
+      "__2": 2707,
+      "__3": 148,
+      "__4": 1447,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2025,
+      "__1": "4/1/2025",
+      "Starts": 60,
+      "__2": 2767,
+      "__3": 179,
+      "__4": 1626,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2025,
+      "__1": "5/1/2025",
+      "Starts": 44,
+      "__2": 2811,
+      "__3": 152,
+      "__4": 1778,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2025,
+      "__1": "6/1/2025",
+      "Starts": 31,
+      "__2": 2842,
+      "__3": 131,
+      "__4": 1909,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2025,
+      "__1": "7/1/2025",
+      "Starts": 23,
+      "__2": 2865,
+      "__3": 162,
+      "__4": 2071,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2025,
+      "__1": "8/1/2025",
+      "Starts": 23,
+      "__2": 2888,
+      "__3": 172,
+      "__4": 2243,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2025,
+      "__1": "9/1/2025",
+      "Starts": 12,
+      "__2": 2900,
+      "__3": 135,
+      "__4": 2378,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2025,
+      "__1": "10/1/2025",
+      "Starts": 11,
+      "__2": 2911,
+      "__3": 165,
+      "__4": 2543,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2025,
+      "__1": "11/1/2025",
+      "Starts": 5,
+      "__2": 2916,
+      "__3": 100,
+      "__4": 2643,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2025,
+      "__1": "12/1/2025",
+      "Starts": 5,
+      "__2": 2921,
+      "__3": 169,
+      "__4": 2812,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2026,
+      "__1": "1/1/2026",
+      "Starts": 1,
+      "__2": 2922,
+      "__3": 110,
+      "__4": 2922,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2026,
+      "__1": "2/1/2026",
+      "Starts": 0,
+      "__2": 2922,
+      "__3": 0,
+      "__4": 2922,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2026,
+      "__1": "3/1/2026",
+      "Starts": 0,
+      "__2": 2922,
+      "__3": 0,
+      "__4": 2922,
+      "__5": "",
+      "__6": ""
+    },
+    {
+      "": 2026,
+      "__1": "4/1/2026",
+      "Starts": 0,
+      "__2": 2922,
+      "__3": 0,
+      "__4": 2922,
+      "__5": "",
+      "__6": ""
+    }] 
+    
+    // Storing label for x-axis
+    let labels = [];
+    data.map((item) => {
+        const inputDate = item.__1;
+        const parsedDate = new Date(inputDate);
+        const options = { year: '2-digit', month: 'long' };
+        const formattedDate = parsedDate.toLocaleString('en-US', options);
+
+        console.log(formattedDate); // Output: June 22
+        labels = labels.concat(formattedDate)
+    })
+    console.log(labels)
+
+    //Storing data for Y-axis of series 1
+    let series1 = [];
+    data.map((item) => {
+        console.log(item.Starts)
+        series1 = series1.concat(item.Starts)
+    })
+    console.log(series1)
+
+    //Storing data from Y-axis of series 3
+    let series3 = [];
+    data.map((item) => {
+        console.log(item.Starts)
+        series3 = series3.concat(item.__3)
+    })
+    console.log(series3)
+
+     //Storing data from Y-axis of series 5
+     let series5 = [];
+     data.map((item) => {
+         console.log(item.Starts)
+         series5 = series5.concat(item.__5)
+     })
+     console.log(series5)
+
+      //Storing data from Y-axis of series 6
+      let series6 = [];
+      data.map((item) => {
+          series6 = series6.concat(item.__6)
+      })
+      console.log(series6)
+
+      //Storing data from Y-axis of series 2
+      let series2 = [];
+      data.map((item) => {
+          series2 = series2.concat(item.__2)
+      })
+      console.log(series2)
+
+       //Storing data from Y-axis of series 4
+       let series4 = [];
+       data.map((item) => {
+           series4 = series4.concat(item.__4)
+       })
+       console.log(series4)
+
+
+
+    // your-script.js
+const ctx = document.getElementById('myChart').getContext('2d');
+
+// Sample data
+
+const barData = [10, 25, 15, 30, 20];
+const lineData = [50, 45, 60, 55, 70];
+
+// Create the bar for series 1
+const barDataset = {
+  label: 'Series1',
+  data: series1,
+  backgroundColor: 'blue', // Bar color
+  borderColor: 'blue',
+  borderWidth: 1,
+  yAxisID: 'bar-y-axis', // Use a separate y-axis for the bar chart
+};
+
+//Create the bar for series 3
+
+const barDataset3 = {
+    label: 'Series3',
+    data: series3,
+    backgroundColor: 'red', // Bar color
+    borderColor: 'red',
+    borderWidth: 1,
+    yAxisID: 'bar-y-axis', // Use a separate y-axis for the bar chart
+  };
+
+  //Create the bar for series 5
+
+const barDataset5 = {
+    label: 'Series5',
+    data: series5,
+    backgroundColor: 'black', // Bar color
+    borderColor: 'black',
+    borderWidth: 1,
+    yAxisID: 'bar-y-axis', // Use a separate y-axis for the bar chart
+  };
+
+   //Create the bar for series 6
+
+const barDataset6 = {
+    label: 'Series6',
+    data: series6,
+    backgroundColor: 'grey', // Bar color
+    borderColor: 'grey',
+    borderWidth: 1,
+    yAxisID: 'bar-y-axis', // Use a separate y-axis for the bar chart
+  };
+
+
+const lineDataset = {
+  label: 'Series2',
+  data: series2,
+  borderColor: 'brown', // Line color
+  borderWidth: 2,
+  backgroundColor: 'rgba(205, 127, 50, 0.2)', 
+  pointRadius: 0,
+  borderSkipped: 'bottom',
+  type : 'line',
+  fill:true // Use a separate y-axis for the line chart
+};
+
+const lineDataset4 = {
+    label: 'Series4',
+    data: series4,
+    borderColor: '#CD7F32',
+    backgroundColor : "#fffddd", // Line color
+    borderWidth: 2,
+    pointRadius: 0,
+    borderSkipped: 'bottom',
+    type : 'line',
+    fill : true // Use a separate y-axis for the line chart
+  };
+
+// Create the chart
+const myChart = new Chart(ctx, {
+  type: 'bar', // Set initial type to bar
+  data: {
+    labels: labels,
+    datasets: [barDataset,lineDataset,barDataset3,barDataset5,lineDataset4,barDataset6],
+  },
+  options: {
+    responsive: true,
+    scales: {
+      y: {
+        id: 'bar-y-axis',
+        beginAtZero: true,
+      },
+      'line-y-axis': {
+        position: 'right',
+        beginAtZero: true,
+      },
+    },
+  },
+});
